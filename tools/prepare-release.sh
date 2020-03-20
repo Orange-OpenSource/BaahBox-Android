26f975de660b211ec4104ce87985825bbf7f1413
Pierre-Yves Lapersonne (Orange) <pierreyves.lapersonne@orange.com>
2020-03-20 10:47:05 +0100

    doc: project - 📝 update license header
    Signed-off-by: Pierre-Yves Lapersonne (Orange) <pierreyves.lapersonne@orange.com>

--- tools/prepare-release.sh
+++ tools/prepare-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #    Orange Baah Box
-#    Copyright (C) 2017 – 2019 Orange SA
+#    Copyright (C) 2017 – 2020 Orange SA
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
